21a7966520d6886d62f7e2016ba2f134a65129f5
ednacardoso <71801131+ednacardoso@users.noreply.github.com>
2025-08-05 13:39:46 -0300

    Update database.php

--- sistema_ouvidoria/config/database.php
+++ sistema_ouvidoria/config/database.php
@@ -1,20 +1,10 @@
 <?php
 
 function getConnection() {
-    $host = 'localhost';         // Endereço do servidor PostgreSQL
-    $dbname = 'ouvidoria'; // Nome do banco
-    $user = 'postgres';       // Usuário do PostgreSQL
-    $password = '1a2badmin#$#';     // Senha do PostgreSQL
-    $port = '5432';              // Porta padrão
-
-    try {
-        $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
-        $pdo = new PDO($dsn, $user, $password, [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-        ]);
+    
         return $pdo;
     } catch (PDOException $e) {
         die("Erro na conexão: " . $e->getMessage());
     }
+
 }
